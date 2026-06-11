506c61f476d536835fc99eecaad788404da5a275
Mayank Mehra <mayankmehra1155mm@gmail.com>
2026-06-11 19:53:54 +0530

    Add advanced SEO tags and schemas

--- gen-sitemap.cjs
+++ gen-sitemap.cjs
@@ -15,20 +15,28 @@ while ((match = regex.exec(appFile)) !== null) {
 // Ensure unique IDs
 const uniqueIds = [...new Set(toolIds)].filter(id => id !== 'privacy-policy'); // We will add privacy policy, wait why not? Yes include it!
 
+const currentDate = new Date().toISOString();
+
 let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
   <url>
     <loc>https://zeroapitools.vercel.app/</loc>
+    <lastmod>\${currentDate}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
+    <xhtml:link rel="alternate" hreflang="en" href="https://zeroapitools.vercel.app/" />
   </url>
 `;
 
 uniqueIds.forEach(id => {
   sitemap += `  <url>
-    <loc>https://zeroapitools.vercel.app/${id}</loc>
+    <loc>https://zeroapitools.vercel.app/\${id}</loc>
+    <lastmod>\${currentDate}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
+    <xhtml:link rel="alternate" hreflang="en" href="https://zeroapitools.vercel.app/\${id}" />
   </url>\n`;
 });
 
