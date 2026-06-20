f5ea0b9e535b323a6a9ffce78e7cf378364ca6dd
Mayank Mehra <mayankmehra1155mm@gmail.com>
2026-06-20 18:14:38 +0530

    Fix CORS origin in api/blogs.js

--- api/blogs.js
+++ api/blogs.js
@@ -64,8 +64,9 @@ function pageToPost(page) {
 
 export default async function handler(req, res) {
   // CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Origin', 'https://zeroapitools.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   try {
